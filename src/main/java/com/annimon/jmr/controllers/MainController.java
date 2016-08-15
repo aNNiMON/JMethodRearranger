596be344bed2a6477ec71645abfaef059df920ba
Victor <annimon119@gmail.com>
2016-08-15 18:51:30 +0300

    First working prototype

--- src/main/java/com/annimon/jmr/controllers/MainController.java
+++ src/main/java/com/annimon/jmr/controllers/MainController.java
@@ -2,8 +2,14 @@ package com.annimon.jmr.controllers;
 
 import com.annimon.jmr.MethodRearranger;
 import com.annimon.jmr.models.Method;
+import com.annimon.jmr.views.MethodCell;
+import com.github.javaparser.ast.CompilationUnit;
 import java.net.URL;
+import java.util.Comparator;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -34,21 +40,37 @@ public class MainController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+        panelMethods.disableProperty().bind(Bindings.isEmpty(lvMethods.getItems()));
+        lvMethods.setCellFactory(param -> new MethodCell());
+
+        taSource.setText("class Main {\n\n    public void method1() {}\n\n"
+                + "    public void test() {}\n\n"
+                + "    public void method2() {\n        // comment\n    }\n }");
     }
 
     @FXML
     private void handleParse(ActionEvent event) {
-        
+        rearranger = MethodRearranger.from(taSource.getText());
+        lvMethods.getItems().addAll(rearranger.methods()
+                .collect(Collectors.toList())
+        );
     }
 
     @FXML
     private void handleSortAsc(ActionEvent event) {
-        
+        final Comparator<Method> byName = Comparator.comparing(m -> m.getMethod().getName());
+        final Comparator<Method> byParametersCount = Comparator.comparing(m -> m.getMethod().getParameters().size());
+
+        final List<Method> sortedMethods = lvMethods.getItems().stream()
+                .sorted(byName.thenComparing(byParametersCount))
+                .collect(Collectors.toList());
+        lvMethods.getItems().clear();
+        lvMethods.getItems().addAll(sortedMethods);
     }
 
     @FXML
     private void handleSave(ActionEvent event) {
-        
+        final CompilationUnit cu = rearranger.modifyMethods(lvMethods.getItems());
+        System.out.println(cu);
     }
 }
