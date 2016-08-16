7a7cb69640f47c1b63985145f0f45cb3a2114b57
Victor <annimon119@gmail.com>
2016-08-16 19:01:29 +0300

    Fix typo in fxml name

--- src/main/java/com/annimon/jmr/Main.java
+++ src/main/java/com/annimon/jmr/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Main.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/main.fxml"));
         Scene scene = new Scene(loader.load());
         primaryStage.setTitle("JMethodRearranger");
         primaryStage.setScene(scene);
