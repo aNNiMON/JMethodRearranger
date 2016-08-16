8686435fb35692b6b9eccb094198cbfd7b064c34
Victor <annimon119@gmail.com>
2016-08-16 17:25:11 +0300

    Add Sort model

---
+++ src/main/java/com/annimon/jmr/models/Sort.java
@@ -0,0 +1,38 @@
+package com.annimon.jmr.models;
+
+import java.util.Comparator;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+
+public final class Sort {
+
+    private final BooleanProperty enabledProperty = new SimpleBooleanProperty(false);
+
+    private final String name;
+    private final Comparator<Method> comparator;
+
+    public Sort(String name, Comparator<Method> comparator) {
+        this.name = name;
+        this.comparator = comparator;
+    }
+
+    public BooleanProperty enabledProperty() {
+        return enabledProperty;
+    }
+
+    public boolean isEnabled() {
+        return enabledProperty.get();
+    }
+
+    public void setEnabled(boolean enabled) {
+        enabledProperty.set(enabled);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Comparator<Method> getComparator() {
+        return comparator;
+    }
+}
